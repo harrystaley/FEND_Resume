444d0738a04c004b65e9c228edae7df4aa32364b
Harry Staley <staleyh@gmail.com>
2015-12-19 19:15:37 -0600

    feat: added bio, work, and education info to resume builder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,21 @@
 /* Developed by Harry Staley for project 2 of the Udacity Front End Web Devlopmer Nanodegree */
 /* This javascript file contains elements of my resume */
-var name = "Harry Staley";
-var role = "Web Developer and Data Analyst";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedName, formattedRole);
+
+// Variable Declarations
+
+// biographical information 
+var bio = {
+	'name' : 'Harry Staley',
+	'role' : 'Web Developer and Data Analyst',
+	'contactMe' : {
+		'mobile' : '330-718-1876',
+		'email' : 'staleyh@gmail.com',
+	},
+  'welcomeMessage' : 'Welcome to my online resume.',
+	'bioPic' : 'images/TBHF_Photo.jpg'
+  // add more bio information here
+};
+
+
 
